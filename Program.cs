31984896081300203ff6525bfcf910353fb51308
Onder B <yazilimdonanim35@hotmail.com>
2022-09-18 00:35:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000000;
+            double faizOrani = 3.45;
+            bool sistemeGirisYapmisMi = false;
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else 
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+           
+
+
+        }
+    }
+}
